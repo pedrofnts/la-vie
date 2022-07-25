ded4b4525e4f0c9275dcd0fe76b539700fba1dad
shinkr99 <felipeshinkr@gmail.com>
2022-07-24 21:18:26 -0300

    feat: adds editing

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,6 @@ routes.get('/psicologos', PsicologosController.index);
 routes.get('/psicologos/:id', PsicologosController.show);
 routes.post('/psicologos', PsicologosController.store);
 routes.delete('/psicologos/:id', PsicologosController.delete);
+routes.put('/psicologos/:id', PsicologosController.update);
 
 module.exports = routes;
