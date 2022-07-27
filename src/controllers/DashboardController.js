60e07c3ae7a31a76a84383b87e2cdb13f8d83cd8
Pedro Fontes <pedro7fontes@gmail.com>
2022-07-27 00:09:05 -0300

    feat: adds dashboard

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,27 @@
+const Atendimentos = require('../models/Atendimentos');
+const Psicologos = require('../models/Psicologos');
+const Pacientes = require('../models/Pacientes');
+
+module.exports = { 
+  async pacCount(req, res) {
+    const numPac = await Pacientes.count();
+    
+    return res.status(200).json(numPac);
+  },
+  async atdCount(req, res) {
+    const numAtd = await Atendimentos.count();
+
+    return res.status(200).json(numAtd);
+  },
+  async psiCount(req, res) {
+    const numPsi = await Psicologos.count();
+
+    return res.status(200).json(numPsi)
+  },
+  async average(req, res) {
+    const atendimentos = await Atendimentos.count();
+    const psicologos = await Psicologos.count();
+
+    return res.status(200).json(atendimentos/psicologos)
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const PsicologosController = require('./controllers/PsicologosController');
 const PacientesController = require('./controllers/PacientesController');
 const AtendimentosController = require('./controllers/AtendimentosController');
-const authController = require('./controllers/authController')
-const psicologoCreateValidation = require('./validations/psicologos/create')
-const authLoginValidation = require('./validations/auth/login')
-const auth = require('./middlewares/auth')
+const DashboardController = require('./controllers/DashboardController');
+const authController = require('./controllers/authController');
+const psicologoCreateValidation = require('./validations/psicologos/create');
+const authLoginValidation = require('./validations/auth/login');
+const auth = require('./middlewares/auth');
 
 const routes = express.Router();
 
@@ -25,6 +26,11 @@ routes.put('/pacientes/:id', PacientesController.update);
 routes.get('/atendimentos', AtendimentosController.index);
 routes.get('/atendimentos/:id', AtendimentosController.show);
 routes.post('/atendimentos', auth, AtendimentosController.store);
+
+routes.get('/dashboard/pacientes', DashboardController.pacCount);
+routes.get('/dashboard/atendimentos', DashboardController.atdCount);
+routes.get('/dashboard/psicologos', DashboardController.psiCount);
+routes.get('/dashboard/atendimentos/media', DashboardController.average);
 
 routes.post('/login', authLoginValidation, authController.store);
 
