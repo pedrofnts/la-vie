177d2e2d15a16956301eda58cf3fba10bb29ee91
Pedro Fontes <pedro7fontes@gmail.com>
2022-07-28 18:08:52 -0300

    feat: heroku deployment

--- src/server.js
+++ src/server.js
@@ -11,4 +11,8 @@ app.use(express.json());
 app.use(routes);
 app.use(handleError);
 
-app.listen(3333);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
