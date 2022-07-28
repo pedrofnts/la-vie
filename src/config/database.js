47fa0220b75b472f3e7db658ad32c1d2529ec24e
Pedro Fontes <pedro7fontes@gmail.com>
2022-07-28 19:03:34 -0300

    feat: cleardb deployment

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  HOST: "us-cdbr-east-06.cleardb.net",
+  USER: "be5ec547bcb702",
+  PASSWORD: "7fbba3a2",
+  DB: "heroku_e214d62439cf1c8",
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+}
