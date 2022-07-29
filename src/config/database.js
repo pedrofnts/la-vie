a4fe897a53672078d4558612acdf1c9703883878
Pedro Fontes <pedro7fontes@gmail.com>
2022-07-28 21:59:12 -0300

    feat: heroku db change

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,7 @@
 module.exports = {
-  development: {
-    client: "mysql",
-    connection: {
-      host: "127.0.0.1",
-      user: "root",
-      database: "auth"
+    dialect: "mysql",
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "be5ec547bcb702",
+    password: "7fbba3a2",
+    database: "heroku_e214d62439cf1c8"
     }
-  },
-  production: {
-    client: "mysql",
-    connection: {
-      host: "us-cdbr-east-06.cleardb.net",
-      user: "be5ec547bcb702",
-      password: "7fbba3a2",
-      database: "heroku_e214d62439cf1c8"
-    }
-  }
-}
