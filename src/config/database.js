7cd4b6086eb392c2c4a9fe2319f6388d729e6f89
Pedro Fontes <pedro7fontes@gmail.com>
2022-07-28 22:31:33 -0300

    agora vai

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,11 @@
 module.exports = {
-    dialect: "mysql",
-    host: "us-cdbr-east-06.cleardb.net",
-    user: "be5ec547bcb702",
-    password: "7fbba3a2",
-    database: "heroku_e214d62439cf1c8"
+    dialect: 'mysql',
+    host: 'us-cdbr-east-06.cleardb.net',
+    username: 'be5ec547bcb702',
+    password: '7fbba3a2',
+    database: 'heroku_e214d62439cf1c8',
+    define: {
+        timestamps: true,
+        underscored: true,
+        },
     }
